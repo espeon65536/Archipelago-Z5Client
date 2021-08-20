f633423774fe3ee41920eb3ef684ab5c34af7fc3
Chris Wilson <chris@legendserver.info>
2021-08-20 18:06:04 -0400

    Remove 500ms timeout from n64Interval

--- assets/serverSocket.js
+++ assets/serverSocket.js
@@ -232,11 +232,6 @@ const connectToServer = async (address) => {
               }
             }
 
-            // TODO: DELETE THIS LINE
-            // This is a workaround for a race condition caused by the ROM's assembly. Espeon is currently working
-            // on correcting that race condition.
-            await new Promise((resolve) => setTimeout(resolve,500));
-
             // Interval complete, allow a new run
             n64IntervalComplete = true;
           });
