eb3008ed8e396905a778fb71dd196ecc601a2bd4
Chris Wilson <chris@legendserver.info>
2021-10-03 04:50:20 -0400

    Fix typo in LUA script version

--- lua/ootMulti.lua
+++ lua/ootMulti.lua
@@ -1,4 +1,4 @@
-local script_version = '2021-10-13' -- Should be the last modified date
+local script_version = '2021-10-03' -- Should be the last modified date
 
 --------------------------------------------------
 -- Heavily modified form of RiptideSage's tracker
